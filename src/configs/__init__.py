9d2b307873d261f604fd351c1b658800160e9c4c
Arthur Koba <caprimores@gmail.com>
2024-11-08 12:41:13 +0300

    add configs

---
+++ src/configs/__init__.py
@@ -0,0 +1,4 @@
+from .defaults import RESOURCES_PATH, SESSIONS_PATH
+
+RESOURCES_PATH.mkdir(exist_ok=True)
+SESSIONS_PATH.mkdir(exist_ok=True)
---
+++ src/configs/defaults.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_SOURCES_PATH = Path(__file__).parent.parent
+RESOURCES_PATH = Path(PROJECT_SOURCES_PATH.parent, "resources")
+SESSIONS_PATH = Path(RESOURCES_PATH, "sessions")
+
+
+
