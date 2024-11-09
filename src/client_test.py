bd6ae587f9ee7709996c8df73d0d6822b51edec8
Arthur Koba <caprimores@gmail.com>
2024-11-09 12:29:05 +0300

    base template

---
+++ src/client_test.py
@@ -0,0 +1,31 @@
+from asyncio import run
+
+from aiohttp import ClientSession
+from telethon.tl.functions.messages import RequestAppWebViewRequest
+from telethon.tl.types import InputBotAppShortName, InputUser
+
+from telegram import TFAFrontendClient
+
+
+async def main():
+    url = "http://127.0.0.1:8000/api/client/send_raw_request"
+    async with ClientSession() as session:
+
+        client = TFAFrontendClient(url=url, session=session)
+        peer = await client.get_entity("BlumCryptoBot")
+        data = await client(RequestAppWebViewRequest(
+            peer=peer,
+            app=InputBotAppShortName(bot_id=InputUser(peer.id, access_hash=peer.access_hash), short_name="app"),
+            platform='android',
+            write_allowed=True,
+            start_param=None
+        ))
+        print(data)
+
+
+if __name__ == '__main__':
+    try:
+        run(main())
+    except SystemExit:
+        pass
+
