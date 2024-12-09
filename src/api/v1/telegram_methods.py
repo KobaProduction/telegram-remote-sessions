6318975c8887091a0977adaa70f77161cf631ca3
ArthurKoba <caprimores@gmail.com>
2024-12-09 05:33:00 +0300

    delete proxy from trs frontend client, release throw RPCError from backend

--- src/api/v1/telegram_methods.py
+++ src/api/v1/telegram_methods.py
@@ -5,12 +5,12 @@ from fastapi import APIRouter, Depends, status
 from fastapi.responses import Response
 from fastapi.requests import Request
 from starlette.responses import JSONResponse
+from telethon.errors import RPCError
 
 from context import context
 from trs import TRSBackendClient, convert_to_pre_json, convert_from_pre_json, convert_objects_from_dict
 
-
-router = APIRouter(prefix="/client", tags=["Client Methods"])
+router = APIRouter(prefix="/client")
 
 
 @router.post("/send_pickle_request")
@@ -22,9 +22,14 @@ async def send_pickle_request(request: Request, client: TRSBackendClient = Depen
         )
     data = await request.body()
     if not data:
-        return JSONResponse({"error" : "Empty body"}, status_code=status.HTTP_400_BAD_REQUEST)
+        return JSONResponse({"error": "Empty body"}, status_code=status.HTTP_400_BAD_REQUEST)
     async with client:
-        result = await client(pickle_loads(data), ordered=False)
+        try:
+            result = await client(pickle_loads(data), ordered=False)
+        except RPCError as e:
+            result = e
+        except Exception:
+            raise
         return Response(content=pickle_dumps(result), media_type="application/python-pickle")
 
 
--- src/trs/clients/_frontend.py
+++ src/trs/clients/_frontend.py
@@ -1,4 +1,3 @@
-import typing
 from aiohttp import ClientSession
 from pickle import dumps as pickle_dump, loads as pickle_loads
 
@@ -12,13 +11,8 @@ class TRSFrontendClient(
     AccountMethods, AuthMethods, DownloadMethods, DialogMethods, ChatMethods, BotMethods,
     MessageMethods, UploadMethods, ButtonMethods, UpdateMethods, MessageParseMethods, UserMethods
 ):
-    _proxy: typing.Optional[str]
-    _session: ClientSession
-    _url = str
-
-    def __init__(self, url: str, session: ClientSession, *, proxy: typing.Union[tuple, dict] = None):
+    def __init__(self, url: str, session: ClientSession):
         self._session = session
-        self._proxy = proxy
         self._url = url
         self._mb_entity_cache = MbEntityCache()
 
@@ -28,13 +22,9 @@ class TRSFrontendClient(
         async with self._session.post(self._url, headers=headers, data=dump) as response:
             if response.status == 200:
                 data = await response.content.read()
-                return pickle_loads(data)
+                result = pickle_loads(data)
+                if isinstance(result, Exception):
+                    raise result
+                return result
             error = await response.json()
             raise TypeError(error.get("error"))
-
-    async def _call(self, sender, request, ordered=False, flood_sleep_threshold=None):
-        print(sender, request, ordered, flood_sleep_threshold)
-
-    @property
-    def proxy(self) -> typing.Optional[str]:
-        return self._proxy
