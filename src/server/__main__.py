2276462fea7cab8649da859b29dcf3dc8f4bc041
ArthurKoba <caprimores@gmail.com>
2024-12-11 06:01:00 +0300

    refactor server

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -2,12 +2,15 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from api import api_router
 from configs import SERVER_PORT, SERVER_HOST
+from server.api import api_router
+
+SERVER_VERSION = "0.0.3"
+SERVER_NAME = "Telegram Remote Sessions Server"
 
 app = FastAPI(
-    title="Telegram Remote Sessions Server",
-    version="0.0.2"
+    title=SERVER_NAME,
+    version=SERVER_VERSION
 )
 
 app.include_router(api_router)
@@ -16,6 +19,15 @@ app.include_router(api_router)
 @app.get("/")
 async def read_root():
     return RedirectResponse("/docs")
+
+
+@app.get("/status")
+async def get_status():
+    return {
+        "status": "ok",
+        "version": SERVER_VERSION,
+        "server_name": SERVER_NAME
+    }
 
 
 if __name__ == '__main__':
