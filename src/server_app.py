749fd2461a27f4b20c61aea050f628d4908a2854
ArthurKoba <caprimores@gmail.com>
2024-12-12 22:34:00 +0300

    add CORS middleware

--- src/server_app.py
+++ src/server_app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 from configs import SERVER_HOST, SERVER_PORT
@@ -8,6 +9,14 @@ from server import api_router, SERVER_NAME, SERVER_VERSION
 app = FastAPI(
     title=SERVER_NAME,
     version=SERVER_VERSION
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
